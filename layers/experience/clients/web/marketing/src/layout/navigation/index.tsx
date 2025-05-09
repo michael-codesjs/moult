ab1bc1c16f2672eb44427b452cc7c99da70bbb7c
Michael Phiri <michael.codesjs@gmail.com>
2025-05-09 04:15:27 +0200

    minor: update sign-in url

--- layers/experience/clients/web/marketing/src/layout/navigation/index.tsx
+++ layers/experience/clients/web/marketing/src/layout/navigation/index.tsx
@@ -29,7 +29,7 @@ export const BottomNavigation = () => {
             <span className="text-xs text-white mt-2">Get the app</span>
           </Link>
           
-          <Link href="/auth/signin" className="flex flex-col items-center">
+          <Link href="/auth/in" className="flex flex-col items-center">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
             </svg>
